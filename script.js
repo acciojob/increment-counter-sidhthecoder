abafd56bf10d3331234178fe6df7081f98b291ba
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-07-30 22:04:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let counter=document.getElementById("counter");
 let incrementBtn=document.getElementById("incrementBtn");
 let count=0;
 function increment(){
+	alert(count);
 	count=count+1;
 	counter.innerText=count
-	alert(count);
+	
 }
 
 incrementBtn.addEventListener("click",increment);
